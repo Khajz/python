bcb07066af1faec3d2f8084ca0162ebc73986581
Enes <149907217+enes-github@users.noreply.github.com>
2024-04-14 16:36:25 +0300

    Update wordcloudveriDuzenlenmis.py

--- csv/wordcloudveriDuzenlenmis.py
+++ csv/wordcloudveriDuzenlenmis.py
@@ -1,9 +1,9 @@
 import csv
 
-with open('csvAlistirmalari\\wordcloudveri.csv','r',newline='') as dosya:
+with open('csv\\wordcloudveri.csv','r',newline='') as dosya:
     csvokuyucu=csv.reader(dosya)
     for row in csvokuyucu:
         if 'CNSV' in row:
-            with open('csvAlistirmalari\\wordcloudveri_duzenlenmis.csv','a',newline='') as dosya1:
+            with open('csv\\wordcloudveri_duzenlenmis.csv','a',newline='') as dosya1:
                 writer=csv.writer(dosya1)
                 writer.writerow(row)
